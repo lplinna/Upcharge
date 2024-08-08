8f16db3daa609697e07c7236a40fb2cf1ec74dd9
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-08-08 15:03:02 -0500

    More "intuitive" jumping

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,22 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@export var move_speed: float = 7
+@export var move_speed: float = 700/2
+@export var gravity_intensity: float = 300*4
+@export var jump_speed = 2000/8
+
 
 func _physics_process(delta):
-	var move_dir: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down", 0.1)
-	move_dir *= move_speed
-	move_and_collide(move_dir)
+	velocity.y += gravity_intensity * delta
+
+	if is_on_floor():
+		if Input.is_action_just_released("move_up"):
+			velocity.y = -jump_speed
+			print("JUMPING")
+		var move_dir = Input.get_axis("move_left","move_right")
+		velocity.x = move_dir * move_speed
+		
+		
+	print(velocity.y)
+	move_and_slide()
+
 	
