bf4d0e4e38c82d6b8ff6a93c2f36ba181b10f609
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-08-08 15:25:04 -0500

    Added jump-charge mechanic

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,18 +5,35 @@ extends CharacterBody2D
 @export var jump_speed = 2000/8
 
 
+var time_jump_pressed: float = 0
+
+var old_velx: float = 0
+
+
 func _physics_process(delta):
 	velocity.y += gravity_intensity * delta
 
 	if is_on_floor():
 		if Input.is_action_just_released("move_up"):
-			velocity.y = -jump_speed
-			print("JUMPING")
+			var jump_time_elapsed = Time.get_ticks_msec() - time_jump_pressed
+			var jump_factor = clamp(jump_time_elapsed/250,1,2)
+			print(jump_factor)
+			velocity.y = -jump_speed * jump_factor
+		if Input.is_action_just_pressed("move_up"):
+			time_jump_pressed = Time.get_ticks_msec()
+			
+			
 		var move_dir = Input.get_axis("move_left","move_right")
 		velocity.x = move_dir * move_speed
+		if velocity.x != 0:
+			old_velx = velocity.x
+	else:
+		velocity.x  = old_velx
+	
+	if is_on_wall():
+		old_velx = 0
 		
-		
-	print(velocity.y)
+	#print(velocity.y)
 	move_and_slide()
 
 	
