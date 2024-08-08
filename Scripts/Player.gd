e84b057916f9977cb7bb375eae96a74d0143bedc
Jordan Levy <jordan.levy@bigspark.dev>
2024-08-08 13:29:46 -0700

    basic pop up over player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,16 @@
 extends RigidBody2D
 
 @export var move_speed: float = 7
+@export var floor_y: float = 560
 
-func _physics_process(delta):
+@onready var PopUp = $PopUp
+
+func _physics_process(_delta):
 	var move_dir: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down", 0.1)
 	move_dir *= move_speed
 	move_and_collide(move_dir)
-	
+
+func _process(_delta: float):
+	if !PopUp.visible and position.y > floor_y:
+		print('fall detected')
+		PopUp.visible = true
