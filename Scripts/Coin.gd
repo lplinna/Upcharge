576fed7a4057d921e5e70e8721d4bbf06f88e0be
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-08-13 16:19:26 -0500

    Coin cannot be repeatedly obtained now

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -1,9 +1,10 @@
 extends RigidBody2D
 class_name Coin
 
+var claimed: bool = false
 
 func _on_area_2d_body_entered(body):
-	if body is Player:
+	if body is Player and not claimed:
 		jump_collect(body)
 
 ## The coin jumps into the air and is collected
@@ -11,6 +12,7 @@ func _on_area_2d_body_entered(body):
 ## Parameters:
 ##		player - the player that collected this coin.
 func jump_collect(player: Player):
+	claimed = true
 	collision_layer = 0
 	collision_mask = 0
 	apply_impulse(Vector2(0,-300.0),Vector2(0,0))
