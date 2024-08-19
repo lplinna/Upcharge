9b01e277066cea35489b1400b69235d1bf9fe737
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-08-19 16:03:56 -0500

    Modified Jump King Style

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -3,24 +3,10 @@ extends Camera2D
 ## Reference to player.
 @export var player_ref: Player
 
-## Enum for camera behavior.
-enum camera_mode {
-	JUMP_KING,
-	SIMPLE,
-	X_LOCKED
-}
-
-## Chosen camera behavior.
-@export var current_mode: camera_mode
-
 func _process(delta):
-	match current_mode:
-		camera_mode.JUMP_KING:
-			var top_screen = self.position.y - (0.5 * get_viewport_rect().size.y)
-			var bottom_screen =  self.position.y + (0.5 * get_viewport_rect().size.y)
-			if player_ref.position.y < top_screen or player_ref.position.y > bottom_screen:
-				position.y = player_ref.position.y
-		camera_mode.SIMPLE:
-			position = player_ref.position
-		camera_mode.X_LOCKED:
-			position.y = player_ref.position.y
+	var half_screen = (0.5 * get_viewport_rect().size.y)
+	var top_screen = self.position.y - half_screen
+	var bottom_screen =  self.position.y + half_screen
+	if player_ref.position.y < top_screen or player_ref.position.y > bottom_screen:
+		position.y = player_ref.position.y - (half_screen * 0.7)
+
