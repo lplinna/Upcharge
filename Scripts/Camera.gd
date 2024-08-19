101c729eab06407ac3701c41bbda711c90638ec2
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-08-19 15:53:18 -0500

    Added contingency for falling back down (more like forgot it)

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -17,8 +17,8 @@ func _process(delta):
 	match current_mode:
 		camera_mode.JUMP_KING:
 			var top_screen = self.position.y - (0.5 * get_viewport_rect().size.y)
-			var leonine = abs(player_ref.position.y - position.y)
-			if player_ref.position.y < top_screen:
+			var bottom_screen =  self.position.y + (0.5 * get_viewport_rect().size.y)
+			if player_ref.position.y < top_screen or player_ref.position.y > bottom_screen:
 				position.y = player_ref.position.y
 		camera_mode.SIMPLE:
 			position = player_ref.position
