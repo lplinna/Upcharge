7677e43868dd7153182b37681a61479335605797
CraftyNinjaSquad <ngardner4843@gmail.com>
2024-08-21 13:27:29 -0700

    Added player jump sound

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,6 +12,9 @@ class_name Player
 @export var fall_threshold: float = 100
 
 @onready var PopUp = $PopUp
+@onready var playerSounds = $PlayerSounds
+
+const PlayerJumpSound = preload("res://Resources/player_jump.wav")
 
 ## Movement variables
 var time_jump_pressed: float = 0
@@ -53,6 +56,9 @@ func _physics_process(delta):
 		if Input.is_action_just_released("move_up"):
 			var jump_time_elapsed = Time.get_ticks_msec() - time_jump_pressed
 			var jump_factor = clamp(jump_time_elapsed/250,1,2)
+			playerSounds.stream = PlayerJumpSound
+			playerSounds.pitch_scale = (3.0 - jump_factor)
+			playerSounds.play()
 			#print(jump_factor)
 			velocity.y = -jump_speed * jump_factor
 		elif Input.is_action_just_pressed("move_up"):
