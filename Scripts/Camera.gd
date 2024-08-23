1f065a8453f8ba10cbe1afe1cf1bdc1421ba6c6d
toppop7 <doofenschmad@gmail.com>
2024-08-23 17:20:44 -0500

    Really simple fix

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -5,7 +5,7 @@ extends Camera2D
 
 func _process(delta):
 	var half_screen = (0.5 * get_viewport_rect().size.y)
-	var top_screen = self.position.y - half_screen
+	var top_screen = self.position.y - half_screen * 0.5
 	var bottom_screen =  self.position.y + half_screen
 	if player_ref.position.y < top_screen or player_ref.position.y > bottom_screen:
 		position.y = player_ref.position.y - (half_screen * 0.7)
