35d1fa9fd4271073a9df41b14cd3ab091de35b92
Jordan Levy <jordan.levy@bigspark.dev>
2024-09-04 15:05:45 -0700

    file cleanup; hook up level to menu

---
+++ Scenes/Level/Components/Pipes/pipe_curve.gd
@@ -0,0 +1,40 @@
+@tool
+extends Node2D
+class_name PipeCurve
+
+var top_left_texture = preload("res://Assets/Pipes/JPipes/TopL.png")
+var top_right_texture = preload("res://Assets/Pipes/JPipes/TopR.png")
+var bottom_left_texture = preload("res://Assets/Pipes/Jpipes/BotL.png")
+var bottom_right_texture = preload("res://Assets/Pipes/JPipes/BotR.png")
+
+enum Orientation {
+	TOP_LEFT,
+	TOP_RIGHT,
+	BOTTOM_LEFT,
+	BOTTOM_RIGHT
+}
+
+@export var orientation: Corner:
+	set(value):
+		orientation = value
+		update_texture()
+	get:
+		return orientation
+
+func _ready():
+	update_texture()
+
+func update_texture():
+	match orientation:
+		Orientation.TOP_LEFT:
+			$Sprite2D.texture = top_left_texture
+			$StaticBody2D/CollisionPolygon2D.rotation = 0
+		Orientation.TOP_RIGHT:
+			$Sprite2D.texture = top_right_texture
+			$StaticBody2D/CollisionPolygon2D.rotation = deg_to_rad(90)
+		Orientation.BOTTOM_LEFT:
+			$Sprite2D.texture = bottom_left_texture
+			$StaticBody2D/CollisionPolygon2D.rotation = deg_to_rad(270)
+		Orientation.BOTTOM_RIGHT:
+			$Sprite2D.texture = bottom_right_texture
+			$StaticBody2D/CollisionPolygon2D.rotation = deg_to_rad(180)
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -5,7 +5,7 @@ extends Control
 @onready var credits = $AspectRatioContainer/Credits
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level/level_01.tscn")
 
 
 func _on_how_to_pressed():
