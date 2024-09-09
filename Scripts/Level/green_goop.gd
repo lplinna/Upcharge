264d377bbc80c26952f8602c580de98ede18bf65
CraftyNinjaSquad <ngardner4843@gmail.com>
2024-09-08 21:43:52 -0700

    Fixed goop issue

--- Scripts/Level/green_goop.gd
+++ Scripts/Level/green_goop.gd
@@ -3,7 +3,9 @@ extends Node2D
 func _on_area_2d_body_entered(body):
 	if body is Player:
 		body.horizontal_lethargy = 0.05 #the lower the value the more slipery the goop.
+		body.wet_floor = true
 
 func _on_area_2d_body_exited(body):
 	if body is Player:
 		body.horizontal_lethargy = 0.2 #the given default value in the player.gd script.
+		body.wet_floor = false
