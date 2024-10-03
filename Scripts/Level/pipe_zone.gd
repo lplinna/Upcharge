cb0488b556c38f728cdacc7f5f0b399c50d4dea9
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-10-03 12:41:26 -0500

    Kind of working?

--- Scripts/Level/pipe_zone.gd
+++ Scripts/Level/pipe_zone.gd
@@ -6,23 +6,37 @@ class_name PipeZone
 
 var neighbors: Array[PipeZone] 
 
+var stop_eating: bool = false
 var closed: bool = true
-var selectable: bool = true
+var selectable: bool = false
+var stored_player: Player = null
 
 
 func _ready():
+	stored_player = get_tree().get_first_node_in_group("Player")
 	for node in get_parent().get_children():
 		if node is PipeZone and node != self:
 			neighbors.append(node)
 	print(neighbors)
 
 func move_player_here():
-	var player = get_tree().get_first_node_in_group("Player")
-	player.global_position = self.global_position
+	stop_eating = true
+	get_tree().root.add_child(stored_player)
+	stored_player.global_position = self.global_position
 
+func make_neighbors_selectable():
+	for neighbor in neighbors:
+		neighbor.selectable = true
+		
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == 1 and event.pressed == true:
 		if selectable:
 			closed = false
 			move_player_here()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player and not stop_eating:
+		body.get_parent().remove_child(body)
+		make_neighbors_selectable()
