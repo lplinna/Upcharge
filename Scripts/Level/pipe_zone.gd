0d6cc754c1479a8148d03f1e4bea32b1219a3d2a
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-10-03 19:24:12 -0500

    Player should be able to press E to enter pipe, unimplemented

--- Scripts/Level/pipe_zone.gd
+++ Scripts/Level/pipe_zone.gd
@@ -8,7 +8,7 @@ var neighbors: Array[PipeZone]
 
 var stop_eating: bool = false
 var closed: bool = true
-var selectable: bool = false
+var selectable: bool = true
 var stored_player: Player = null
 
 
@@ -20,15 +20,17 @@ func _ready():
 	print(neighbors)
 
 func move_player_here():
+	stored_player.process_mode = Node.PROCESS_MODE_INHERIT
 	stored_player.global_position = self.global_position
-	get_tree().root.add_child(stored_player)
+	await get_tree().create_timer(1.0).timeout
+	stop_eating = false
+	selectable = false
 
 
 func make_neighbors_selectable():
 	for neighbor in neighbors:
 		neighbor.selectable = true
 		neighbor.stop_eating = true
-		
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == 1 and event.pressed == true:
@@ -41,5 +43,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if stop_eating:
 		return
 	if body is Player:
-		body.get_parent().remove_child(body)
+		stored_player = body
+		stored_player.process_mode = Node.PROCESS_MODE_DISABLED
 		make_neighbors_selectable()
