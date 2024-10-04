e7c5cfe7eb675bf130123bc5cf3efa1f1f9517d1
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-10-03 19:49:15 -0500

    Culling and goofiness

--- Scripts/Level/pipe_zone.gd
+++ Scripts/Level/pipe_zone.gd
@@ -10,6 +10,7 @@ var stop_eating: bool = false
 var closed: bool = true
 var selectable: bool = true
 var stored_player: Player = null
+var player_there: bool = false
 
 
 func _ready():
@@ -17,35 +18,20 @@ func _ready():
 	for node in get_parent().get_children():
 		if node is PipeZone and node != self:
 			neighbors.append(node)
-	print(neighbors)
 
 func move_player_here():
-	if stored_player != null:
-		stored_player.global_position = self.global_position
-
-func make_neighbors_selectable():
-	for neighbor in neighbors:
-		neighbor.selectable = true
-		neighbor.stop_eating = true
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == 1 and event.pressed == true:
-		if selectable:
-			closed = false
-			move_player_here()
+	stored_player.global_position = self.global_position
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("return"):
+	if Input.is_action_just_pressed("return") and player_there:
 		var next_neighbor = neighbors.pick_random()
 		next_neighbor.move_player_here()
-		stored_player = null
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
-		stored_player = body
-		make_neighbors_selectable()
+		player_there = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body == stored_player:
-		stored_player = null
+	if body is Player:
+		player_there = false
