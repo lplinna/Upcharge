aa715ec9fbb8c9f2341b895272be0dd0bf2c9203
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-10-18 15:46:59 -0500

    Added clause to win the game if you have the cheese

--- Scripts/Level/WinZone.gd
+++ Scripts/Level/WinZone.gd
@@ -6,7 +6,7 @@ var win_state: bool = false
 
 func _on_area_2d_body_entered(body):
 	if body is Player and not win_state:
-		if body.coins >= coins_to_win:
+		if body.coins >= coins_to_win or body.held_item == 3:
 			win_state = true
 			$CenterContainer/YouWin.visible = true
 			$CenterContainer/NotEnough.visible = false
@@ -17,4 +17,3 @@ func _on_area_2d_body_entered(body):
 			await get_tree().create_timer(2.0).timeout
 			$CenterContainer/NotEnough.visible = false
 			get_tree().change_scene_to_file("res://Scenes/Level/level_01.tscn")
-			
