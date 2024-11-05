960097769d3becc7838a2705fbcc8abf035a91b0
Jordan Levy <jordanlevy96@protonmail.com>
2024-11-05 15:26:04 -0800

    guard placeholder

--- Scripts/Level/rat_guard.gd
+++ Scripts/Level/rat_guard.gd
@@ -22,22 +22,3 @@ func _ready() -> void:
 func _on_speech_zone_area_entered(area: Area2D) -> void:
 	print('speech zone entered')
 	speech_bubble.texture = coin_ask
-
-@export var coins_to_win: int = 4
-
-var win_state: bool = false
-
-## defunct winzone code
-#func _on_area_2d_body_entered(body):
-	#if body is Player and not win_state:
-		#if body.coins >= coins_to_win or body.held_item == 3:
-			#win_state = true
-			#$CenterContainer/YouWin.visible = true
-			#$CenterContainer/NotEnough.visible = false
-			#await get_tree().create_timer(2.0).timeout
-			#get_tree().change_scene_to_file("res://Scenes/Level/MainMenu.tscn")
-		#else:
-			#$CenterContainer/NotEnough.visible = true
-			#await get_tree().create_timer(2.0).timeout
-			#$CenterContainer/NotEnough.visible = false
-			#get_tree().change_scene_to_file("res://Scenes/Level/level_01.tscn")
